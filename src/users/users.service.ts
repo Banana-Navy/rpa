f12ba4d85589355d26df5c6b2b1dc02e174a09c5
omar-mahmoudi <omar@banana-navy.com>
2023-10-10 17:06:36 +0100

    clean dto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserDocument } from './schema/users.schema';
 import { Model } from 'mongoose';
@@ -29,17 +28,5 @@ export class UsersService {
     } catch (error) {
       return error;
     }
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
